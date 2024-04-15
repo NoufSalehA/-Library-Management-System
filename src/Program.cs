a5b6e8984ddd508336e27ebab499daf3771f20d5
NoufSalehA <noufsalehc23@gmail.com>
2024-04-15 14:56:33 +0300

    Basic library setup -1

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,20 @@
-﻿internal class Program
+﻿class Book
+{
+    public string id;
+    public string title;
+    public DateTime createdDate;
+
+
+}
+class User
+{
+    private string id;
+    public string name;
+    public DateTime createdDate;
+}
+class Library { }
+
+internal class Program
 {
     private static void Main()
     {
